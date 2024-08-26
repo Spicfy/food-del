594865cbe9d6a49c985f8710acda52e4fbab185e
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-08-26 02:39:06 -0400

    finished add item and storing food items into admin list

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    (await mongoose.connect('mongodb+srv://bruce:33858627@bruce.yy3td.mongodb.net/food-del')).then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://bruce:33858627@bruce.yy3td.mongodb.net/food-del').then(()=>console.log("DB Connected"));
 }
